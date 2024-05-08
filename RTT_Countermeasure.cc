212753c84ff0171ebebb1bfb046ae05d0561c1ef
Ziyi-Z <160652977+Zavier-Z@users.noreply.github.com>
2024-05-08 00:11:27 -0400

    Update RTT_Countermeasure.cc

--- RTT_Countermeasure.cc
+++ RTT_Countermeasure.cc
@@ -14,14 +14,40 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("WormholeExample");
 
-void ReceivePacket(Ptr<const Packet> p, const Address& addr, Ptr<RttEstimator> rttEstimator)
+class RttLogger
 {
-    Time receivedTime = Simulator::Now();
-    // This is a simplification. In a real scenario, you would track when packets are sent.
-    Time sentTime = Simulator::Now(); // This should actually be tracked properly
-    rttEstimator->Measurement(receivedTime - sentTime);
+public:
+    RttLogger() : m_rttEstimator(CreateObject<RttMeanDeviation>())
+    {
+        m_rttEstimator->Measurement(Seconds(0.1)); 
+    }
 
-    std::cout << receivedTime.GetSeconds() << "\t" << p->GetSize() << "\tRTT: " << rttEstimator->GetEstimate().GetSeconds() << "s\n";
+    void SentPacket(Ptr<Socket> socket, uint32_t bytes)
+    {
+        Time now = Simulator::Now(); 
+        m_sendTimes[socket->GetNode()->GetId()] = now;
+        NS_LOG_INFO("Packet sent at " << now.GetSeconds());
+    }
+
+    void ReceivedPacket(Ptr<Socket> socket, Ptr<const Packet> packet, const Address& from)
+    {
+        Time now = Simulator::Now(); 
+        auto sentTimeIt = m_sendTimes.find(socket->GetNode()->GetId());
+        if (sentTimeIt != m_sendTimes.end())
+        {
+            Time rtt = now - sentTimeIt->second;
+            m_rttEstimator->Measurement(rtt); 
+            NS_LOG_INFO("Received packet at " << now.GetSeconds() << " with RTT: " << rtt.GetSeconds());
+        }
+    }
+
+private:
+    std::map<uint32_t, Time> m_sendTimes; 
+    Ptr<RttEstimator> m_rttEstimator; 
+
+void ReceivePacket(Ptr<const Packet> p, const Address& addr)
+{
+    std::cout << Simulator::Now().GetSeconds() << "\t" << p->GetSize() << "\n";
 }
 
 int main(int argc, char* argv[])
@@ -44,13 +70,14 @@ int main(int argc, char* argv[])
     wifi.SetStandard(WIFI_STANDARD_80211g);
     wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager", "DataMode", StringValue(phyMode), "ControlMode", StringValue(phyMode));
 
-    YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default();
+    YansWifiPhyHelper wifiPhy = YansWifiPhyHelper();
     YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default();
     wifiPhy.SetChannel(wifiChannel.Create());
     wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
-    wifiChannel.AddPropagationLoss("ns3::TwoRayGroundPropagationLossModel", "SystemLoss", DoubleValue(1), "HeightAboveZ", DoubleValue(1.5));
+    wifiChannel.AddPropagationLoss("ns3::TwoRayGroundPropagationLossModel",
+        "SystemLoss", DoubleValue(1),
+        "HeightAboveZ", DoubleValue(1.5));
 
-    // For range near 250m
     wifiPhy.Set("TxPowerStart", DoubleValue(30));
     wifiPhy.Set("TxPowerEnd", DoubleValue(30));
 
@@ -58,7 +85,7 @@ int main(int argc, char* argv[])
     wifiMac.SetType("ns3::AdhocWifiMac");
 
     NetDeviceContainer devices = wifi.Install(wifiPhy, wifiMac, nodes);
-    wifiPhy.EnablePcap("wifi", devices);  // Enable PCAP for WiFi devices
+    wifiPhy.EnablePcap("wifi", devices);
 
     // Enable AODV
     AodvHelper aodv;
@@ -71,7 +98,7 @@ int main(int argc, char* argv[])
     address.SetBase("10.1.1.0", "255.255.255.0");
     Ipv4InterfaceContainer interfaces = address.Assign(devices);
 
-    // Position the nodes accordingly
+    // Position the nodes
     MobilityHelper mobility;
     Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
     for (uint32_t i = 0; i < nodes.GetN(); ++i) {
@@ -81,30 +108,72 @@ int main(int argc, char* argv[])
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     mobility.Install(nodes);
 
-    // RTT Estimators for each node
-    std::vector<Ptr<RttEstimator>> rttEstimators;
-    for (uint32_t i = 0; i < nodes.GetN(); ++i) {
-        Ptr<RttEstimator> estimator = CreateObject<RttMeanDeviation>();
-        rttEstimators.push_back(estimator);
+    Ipv4InterfaceContainer mal_ipcont;
+    // Introduce wormhole if enabled
+    if (enableWormhole)
+    {
+        NodeContainer wormholeNodes(nodes.Get(0), nodes.Get(5));  // Create wormhole between node 0 and 5
+        NetDeviceContainer mal_devices = wifi.Install(wifiPhy, wifiMac, wormholeNodes);
+
+        address.SetBase("10.1.2.0", "255.255.255.0");
+        mal_ipcont = address.Assign(mal_devices);
+
+        AodvHelper malicious_aodv;
+        malicious_aodv.Set("EnableWrmAttack", BooleanValue(true));
+        malicious_aodv.Set("FirstWifiEndOfWormTunnel", Ipv4AddressValue("10.0.2.1"));
+        malicious_aodv.Set("SecondWifiEndOfWormTunnel", Ipv4AddressValue("10.0.2.2"));
+
+        stack.SetRoutingHelper(malicious_aodv);
+        stack.Install(wormholeNodes);
     }
 
-    // Create applications and bind the receive callback with RTT estimator
-    uint16_t port = 9;  // UDP port for echo application
-    UdpEchoServerHelper echoServer(port);
-    for (uint32_t i = 0; i < nodes.GetN(); ++i) {
-        ApplicationContainer serverApp = echoServer.Install(nodes.Get(i));
-        serverApp.Start(Seconds(1.0));
-        serverApp.Stop(Seconds(100.0));
+    // Install applications: UDP echo server and client with RTT monitoring
+    RttLogger rttLogger; // RTT logger
 
-        Ptr<Socket> recvSocket = Socket::CreateSocket(nodes.Get(i), UdpSocketFactory::GetTypeId());
-        recvSocket->Bind(InetSocketAddress(Ipv4Address::GetAny(), port));
-        recvSocket->SetRecvCallback(MakeBoundCallback(&ReceivePacket, rttEstimators[i]));
+    uint16_t echoPort = 9;
+    UdpEchoServerHelper echoServer(echoPort);
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(4)); // Server on node 4
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(100.0));
+
+    Ptr<Socket> ns3UdpSocket = Socket::CreateSocket(nodes.Get(1), UdpSocketFactory::GetTypeId());
+    ns3UdpSocket->SetRecvCallback(MakeCallback(&RttLogger::ReceivedPacket, &rttLogger));
+    ns3UdpSocket->SetSendCallback(MakeCallback(&RttLogger::SentPacket, &rttLogger));
+
+    UdpEchoClientHelper echoClient(interfaces.GetAddress(4), echoPort);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(nodes.Get(1)); // Client on node 1
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(100.0));
+
+    // Flow Monitor
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+    Simulator::Stop(Seconds(100.0));
+
+    if (enableFlowMonitor)
+    {
+        monitor->SerializeToXmlFile("WormholeFlowMonitor.xml", true, true);
     }
-
-    // Normal traffic simulation setup continues...
-    // Further code for setting up clients, wormhole, flow monitors etc.
 
     Simulator::Run();
     Simulator::Destroy();
+
+    // Print per flow statistics
+    monitor->CheckForLostPackets();
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); ++i)
+    {
+        Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(i->first);
+        std::cout << "Flow " << i->first << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
+        std::cout << "  Tx Bytes:   " << i->second.txBytes << "\n";
+        std::cout << "  Rx Bytes:   " << i->second.rxBytes << "\n";
+        std::cout << "  Throughput: " << i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1024 / 1024 << " Mbps\n";
+    }
+
     return 0;
 }
